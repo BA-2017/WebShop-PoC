f695ee4936ee189af7db59166d5dbb957980993a
Simon Rüegg <simon@rueggs.ch>
2017-04-10 22:20:13 +0200

    prepare remote debugging

--- WebShop-PoC/docker-dev.sh
+++ WebShop-PoC/docker-dev.sh
@@ -6,7 +6,5 @@ docker build -t dev-image -f Dockerfile.dev .
 
 docker run -it --rm --name dotnet-dev \
     -v $(pwd):/app \
-    -e ASPNETCORE_URLS="http://*:9000" \
-    -e ASPNETCORE_ENVIRONMENT=Development \
     -p 9000:9000 \
     dev-image
