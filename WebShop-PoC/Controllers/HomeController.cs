9961c79f047b921d012469be7825278f5fe208ad
Simon Rüegg <simon@rueggs.ch>
2017-04-10 21:55:00 +0200

    Add sample logging

--- WebShop-PoC/Controllers/HomeController.cs
+++ WebShop-PoC/Controllers/HomeController.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace WebShop_PoC.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ILogger _logger;
+
+        public HomeController(ILogger<HomeController> logger){
+            _logger = logger;
+        }
         public IActionResult Index()
         {
             return View();
@@ -16,7 +22,7 @@ namespace WebShop_PoC.Controllers
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
-
+            _logger.LogInformation("TEST log");
             return View();
         }
 
