d960223a472c1fb8e8f94c64acfd5b7f43e5e976
Simon Rüegg <simon@rueggs.ch>
2017-04-10 20:42:36 +0200

    Make image name lowercase

--- WebShop-PoC/build.sh
+++ WebShop-PoC/build.sh
@@ -6,21 +6,21 @@ set -e
 APP_NAME=WebShop-PoC
 
 DOCKER_REPO=ba2017
-DOCKER_IMAGE=$DOCKER_REPO"/"$APP_NAME
+DOCKER_IMAGE=$DOCKER_REPO"/"$(echo "$APP_NAME" | tr '[:upper:]' '[:lower:]')
 DOCKER_TAG=$TRAVIS_BUILD_NUMBER
 
 cd $APP_NAME
 
 # Build the builder image
-docker build --force-rm -t build-image -t build-image:$DOCKER_TAG -f Dockerfile.build .
+docker build --force-rm -t build-image -f Dockerfile.build .
 # Create a container from the built image
-docker create --name build-container build-image
+docker create --name build-container --rm build-image
 # Copy binaries
 docker cp build-container:/out ./PublishOutput
 # Build the productin image
-docker build --force-rm -t $DOCKER_IMAGE -f Dockerfile.prod .
+docker build --force-rm -t $DOCKER_IMAGE -t $DOCKER_IMAGE:$DOCKER_TAG -f Dockerfile.prod .
 # Login to docker hub
 docker login -u="$DOCKER_HUB_USER" -p="$DOCKER_HUB_PASSWORD"
 # Push docker image
-docker push ba2017/webshop-poc:$DOCKER_TAG
-docker push ba2017/webshop-poc:latest
+docker push $DOCKER_IMAGE:$DOCKER_TAG
+docker push $DOCKER_IMAGE
